30a5021c74b6d3d0e806bb58de45f038909c431b
FaistDev <ben@faistdev.at>
2018-11-05 10:17:41 +0100

    Startup

---
+++ src/gui/Wetter.java
@@ -0,0 +1,40 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package gui;
+
+/**
+ *
+ * @author Ben
+ */
+public class Wetter {
+
+    private String place;
+    private int sealevel;
+    private float temp;
+    private int humidity;
+
+    public Wetter(String place, int sealevel, float temp, int humidity) throws Exception {
+        this.place = place;
+        this.sealevel = sealevel;
+        setTemp(temp);
+        setHumidity(humidity);
+
+    }
+
+    public void setTemp(float temp) throws Exception {
+        if (temp < -35 || temp > 45) {
+            throw new Exception("Falscher Temperatur-Wert");
+        }
+        this.temp = temp;
+    }
+
+    public void setHumidity(int humidity) throws Exception {
+        if (humidity < 0 || humidity > 100) {
+            throw new Exception("Falscher Luftfeuchtigkeit-Wert");
+        }
+    }
+
+}
---
+++ src/gui/WetterModel.java
@@ -0,0 +1,46 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package gui;
+
+import java.util.ArrayList;
+import javax.swing.table.AbstractTableModel;
+
+/**
+ *
+ * @author Ben
+ */
+public class WetterModel extends AbstractTableModel {
+    private ArrayList<Wetter> wetterstationen = new ArrayList<>();
+    private ArrayList<String> cols = new ArrayList<>();
+    
+    @Override
+    public int getRowCount() {
+        return wetterstationen.size();
+    }
+
+    @Override
+    public int getColumnCount() {
+        return cols.size();
+    }
+
+    @Override
+    public String getColumnName(int column) {
+        return cols.get(column); //To change body of generated methods, choose Tools | Templates.
+    }
+    
+    public void add(Wetter w){
+        wetterstationen.add(w);
+        fireTableDataChanged();
+    }
+
+    @Override
+    public Object getValueAt(int rowIndex, int columnIndex) {
+       return wetterstationen.get(rowIndex);
+    }
+    
+    public void 
+    
+}
